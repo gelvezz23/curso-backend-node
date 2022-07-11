9ef53896b93a8d1d383b8afafb6a8f5653fd3573
gelvezz23 <mao_23_@hotmail.com>
2022-07-11 11:30:52 -0500

    manejo de parametors y query - variables de entorno

--- 04-apirestful/app.js
+++ 04-apirestful/app.js
@@ -1,13 +1,23 @@
 /** @format */
 
 const express = require('express');
+const configFile = require('./config');
+require('dotenv').config();
 
 const app = express();
-const port = 3000;
+
 app.get('/', (req, res) => {
 	res.send('Hola');
 });
 
-app.listen(port, () => {
-	console.log(`listen sever ... ${port}`);
+app.get('/api/:year/:month', (req, res) => {
+	//res.send(req.params); los datos viene en la ruta http://localhost:3000/api/2022/08
+	//res.send(req.query); los datos viene en la ruta despues de ? http://localhost:3000/api/2022/08?hola=hola mundo
+
+	res.send(req.params);
+});
+
+app.listen(configFile.PORT, configFile.HOST, () => {
+	console.log(`listen sever ... ${configFile.PORT}`);
+	console.log(`host ... ${configFile.HOST}`);
 });
